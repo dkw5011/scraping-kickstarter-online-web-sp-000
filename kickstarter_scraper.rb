72cbaee329bcb0c1253b2abbcbb3523ee104340c
Danielle Bergbower <dkw5011@gmail.com>
2020-02-21 02:24:11 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,11 +12,7 @@ require 'pry'
 def create_project_hash
   # write your code here
   # This just opens a file and reads it into a variable
-projects = {}
- 
-  kickstarter.css("li.project.grid_4").each do |project|
-    title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {}
+
 end
 
 # return the projects hash
